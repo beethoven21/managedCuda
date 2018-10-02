fc05755c4cb169989bf1bbe2f8f52e19b74640ca
ken <kenneth.domino@domemtech.com>
2018-10-02 18:23:26 -0400

    (1) Updated generate.sh for Linux. (2) Generated new '.so' files.

--- StubsForLinux/generate.sh
+++ StubsForLinux/generate.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# No clue where this is run, with what installed, in what directory.
+# Fix this now.
+unameOut="$(uname -s)"
+if [[ $unameOut =~ Linux* ]]
+then
+    echo Running on Linux -- good.
+else
+    echo Do not run this on anything but Linux.
+    exit 1
+fi
+
 set -e 
 cd "$(dirname "$0")"
+export vd="10.0"
+export vnd="100"
+export ORIGIN=/usr/local/cuda/lib64
 
 genstub () 
 {
@@ -36,24 +50,24 @@ genstub nvcuda libcuda.so.1 false
 genstub nvml libnvidia-ml.so.1 false
 
 # These libraries are from the CUDA SDK and redistributed in the NuGet packages.
-genstub cublas64_91 libcublas.so.9.1 true
-genstub cufft64_91 libcufft.so.9.1 true
-genstub curand64_91 libcurand.so.9.1 true
-genstub cusolver64_91 libcusolver.so.9.1 true
-genstub cusparse64_91 libcusparse.so.9.1 true
+genstub cublas64_$vnd libcublas.so.$vd true
+genstub cufft64_$vnd libcufft.so.$vd true
+genstub curand64_$vnd libcurand.so.$vd true
+genstub cusolver64_$vnd libcusolver.so.$vd true
+genstub cusparse64_$vnd libcusparse.so.$vd true
 genstub cudnn64_7 libcudnn.so.7.0 true
-genstub nvgraph64_91 libnvgraph.so.9.1 true
-genstub nvrtc64_91 "libnvrtc.so.9.1 libnvrtc-builtins.so" true
-genstub nppc64_91 libnppc.so.9.1 true
-genstub nppial64_91 libnppial.so.9.1 true
-genstub nppicc64_91 libnppicc.so.9.1 true
-genstub nppicom64_91 libnppicom.so.9.1 true
-genstub nppidei64_91 libnppidei.so.9.1 true
-genstub nppif64_91 libnppif.so.9.1 true
-genstub nppig64_91 libnppig.so.9.1 true
-genstub nppim64_91 libnppim.so.9.1 true
-genstub nppist64_91 libnppist.so.9.1 true
-genstub nppisu64_91 libnppisu.so.9.1 true
-genstub nppitc64_91 libnppitc.so.9.1 true
-genstub npps64_91 libnpps.so.9.1 true
+genstub nvgraph64_$vnd libnvgraph.so.$vd true
+genstub nvrtc64_$vnd "libnvrtc.so.$vd libnvrtc-builtins.so" true
+genstub nppc64_$vnd libnppc.so.$vd true
+genstub nppial64_$vnd libnppial.so.$vd true
+genstub nppicc64_$vnd libnppicc.so.$vd true
+genstub nppicom64_$vnd libnppicom.so.$vd true
+genstub nppidei64_$vnd libnppidei.so.$vd true
+genstub nppif64_$vnd libnppif.so.$vd true
+genstub nppig64_$vnd libnppig.so.$vd true
+genstub nppim64_$vnd libnppim.so.$vd true
+genstub nppist64_$vnd libnppist.so.$vd true
+genstub nppisu64_$vnd libnppisu.so.$vd true
+genstub nppitc64_$vnd libnppitc.so.$vd true
+genstub npps64_$vnd libnpps.so.$vd true
 
